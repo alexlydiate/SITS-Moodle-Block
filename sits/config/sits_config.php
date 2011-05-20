1dfe19707faec6f8e2c9462b3b6de866df7fcea1
Alex Lydiate <alexlydiate@gmail.com>
2011-05-20 10:07:40 +0100

    added blank config

---
+++ sits/config/sits_config.php
@@ -0,0 +1,10 @@
+<?php
+//Required - A SITS db user with read access to the live database
+define('SITS_DB_USER', '');
+define('SITS_DB_PASS', '');
+define('SITS_DB_NAME', '');
+//Optional - A SITS db user with read access to a test database, if required
+define('SITS_TEST_DB_USER','');
+define('SITS_TEST_DB_PASS', '');
+define('SITS_TEST_DB_NAME', '');
+?>
